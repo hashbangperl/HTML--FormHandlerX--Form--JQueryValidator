637b330425600d28e77e2b655d62f34ef7812f54
Aaron Trevena <aaron.trevena@gmail.com>
2012-08-17 21:04:07 +0100

    initial import to git

---
+++ lib/HTML/FormHandlerX/Form/JQueryValidator.pm
@@ -0,0 +1,45 @@
+package HTML::FormHandlerX::Form::JQueryValidator;
+use strict;
+use warnings;
+
+=head1 NAME
+
+HTML::FormHandlerX::Form::JQueryValidator - Perl extension for blah blah blah
+
+=head1 VERSION
+
+0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+ use HTML::FormHandler::Moose;
+
+ with HTML::FormHandlerX::Form::JQueryValidator;
+
+=head1 DESCRIPTION
+
+This perl role allows you to re-use some form validation rules with the
+ JQuery Validation plugin (http://docs.jquery.com/Plugins/Validation)
+
+=cut
+
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Aaron Trevena, E<lt>teejay@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Aaron Trevena
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
